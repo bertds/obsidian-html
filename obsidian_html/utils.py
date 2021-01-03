297492022d7eb082643064fb15d5ada9dc294cec
Bert De Sutter <>
2021-01-03 20:22:02 +0100

    link png instead of html

--- obsidian_html/utils.py
+++ obsidian_html/utils.py
@@ -11,7 +11,8 @@ def slug_case(text):
 
 
 def md_link(text, link):
-    if re.search(r".*png$", text, flags=0) is None:
+    return "[" + text + "](" + text + ")"
+    if re.search(r".*png$", text) is None:
         return "[" + text + "](" + link + (".html" if GLOBAL.HTML_LINK_EXTENSIONS else "") + ")"
     else:
         return "[" + text + "](" + text + ")"
